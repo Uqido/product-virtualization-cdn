0b63460945275245533124b6204ecc50107bf20a
Antonio Ruggiero <antonio.ruggiero@uqido.com>
2020-03-30 16:21:15 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+var contentRoot;
+
+function getContentRoot(){
+    contentRoot=document.getElementById("modelContentRoot")
+    
+}
+
+function instantiateModelContent(srcUrl){
+    var modelHtml=`
+                    <div class="lp-element lp-code" id="lp-code-40" style="width: 300px; height: 300px;">
+                            <iframe src="${srcUrl}" frameborder="0" allowfullscreen="" scrolling="no" style="width: 300px; height: 300px;"></iframe>
+                    </div>
+                    `
+    contentRoot.innerHTML+=modelHtml
+}
+
+function parseJson(json){
+    var baseurl=json.baseUrl;
+    var assets=json.assets;
+    getContentRoot();
+    for(asset of assets){
+        instantiateModelContent(`${baseurl}/${asset}.html`);
+    }
+}
+
+
+function loadJson(){
+    //read json
+    fetch("./assets.json")
+    .then(response => response.json())
+    .then(json => parseJson(json))
+}
+
+loadJson();
