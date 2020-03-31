5a69bfbb37dabcacfe4e828fe407fc1922e5ffd5
Nicola Spolador <gench23@gmail.com>
2020-03-31 18:26:38 +0200

    fix geox html, link a modello da fixare

--- js/geox.js
+++ js/geox.js
@@ -1,44 +1,73 @@
 
 var contentRoot;
 
-function getContentRoot(){
-    contentRoot=document.getElementById("modelContentRoot")
-    
+function getContentRoot() {
+    contentRoot = document.getElementById("modelContentRoot")
+
 }
 
-function instantiateModelContent(srcUrl){
-    var modelHtml=`
-                    <div class="lp-element lp-code" id="lp-code-40" style="width: 300px; height: 300px;">
-                            <iframe src="${srcUrl}" frameborder="0" allowfullscreen="" scrolling="no" style="width: 300px; height: 300px;"></iframe>
+function instantiateModelContent(view360url, model3Durl) {
+    var modelHtml = `
+                    <div>
+                    <p class="lp-element lp-code" id="lp-code-40" style="width: 300px; height: 300px;">
+                            <iframe src="${view360url}" frameborder="0" allowfullscreen="" scrolling="no" style="width: 300px; height: 300px;"></iframe>
+                    </p>
+                    <div id="view-3d-model" class="product-details">
+                        <div class="first-line">
+                            <span class="colors">1
+                                colore
+                            </span>
+                            <div class="category">SCARPE CASUAL</div>
+                        </div>
+                        <a title="SYMBOL UOMO"
+                            class="tile_product-name">
+                            SYMBOL UOMO
+                        </a>
+                        <div class="pdp-price">
+                            <div class="pdp-price__details">
+                                <span class="pdp_price__label">Prezzo</span>
+                            </div>
+                            <div class="pdp-price__final">
+                                <span class="price-currency">€</span>99,90
+                            </div>
+                        </div>
+                    </div>
+                    <script>
+                        const button = document.getElementById('view-3d-model');
+
+                        button.addEventListener("click", function () {
+                            window.open("${model3Durl}", "_blank")
+                        }, true)
+                    </script>
                     </div>
                     `
-    contentRoot.innerHTML+=modelHtml
+    contentRoot.innerHTML += modelHtml
 }
 
-function isLocalhost(){
-    if( !window.location.host.replace( /localhost|127\.0\.0\.1/i, '' ) ){ 
-        return false; 
+function isLocalhost() {
+    if (!window.location.host.replace(/localhost|127\.0\.0\.1/i, '')) {
+        return false;
     }
     return true;
 }
 
-function parseJson(json){
-    var baseurl=json.baseUrl;
-    if(isLocalhost())
-        baseurl="";
-    var assets=json.assets_shoes;
+function parseJson(json) {
+    var baseurl = json.baseUrl;
+    if (isLocalhost())
+        baseurl = "";
+    var assets = json.assets_shoes;
     getContentRoot();
-    for(asset of assets){
-        instantiateModelContent(`${baseurl}/${asset}.html`);
+    for (asset of assets) {
+        instantiateModelContent(`${baseurl}/${asset}.html`,`${baseurl}/${asset}Model.html`);
     }
 }
 
 
-function loadJson(){
+function loadJson() {
     //read json
     fetch("./assets.json")
-    .then(response => response.json())
-    .then(json => parseJson(json))
+        .then(response => response.json())
+        .then(json => parseJson(json))
 }
 
 loadJson();
