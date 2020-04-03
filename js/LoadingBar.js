1b9fb619e85dfce3cab9923dc4489f46c54dc027
Antonio Ruggiero <antonio.ruggiero@uqido.com>
2020-04-03 09:58:43 +0200

    posizione iniziale camera, stili loading bar dentro il javascript

--- js/LoadingBar.js
+++ js/LoadingBar.js
@@ -1,12 +1,44 @@
 const mviewer = document.getElementsByTagName("model-viewer")[0];
 
 
-if(mviewer) {
+if (mviewer) {
     mviewer.classList.add("no-default-loading");
     mviewer.innerHTML += `<div class="iframe-loader" id="loading-bar">
         Loading 0%
     <div style="width: 0%;"></div>
-        </div>`;
+        </div>
+    <style>
+     .iframe-loader {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 11;
+        display: block;
+        color: #DDD;
+        font-weight: 700;
+        font-size: 1.2rem;
+        pointer-events: none;
+    }
+    
+    .iframe-loader > div {
+        position: absolute;
+        bottom: -10px;
+        left: 0;
+        width: 100%;
+        height: 3px;
+        background: #DDD;
+    }
+    
+    .iframe-loader.hidden {
+        animation: none;
+        opacity: 0;
+        -webkit-transition: all 0.25s ease-out;
+        -moz-transition: all 0.25s ease-out;
+        -o-transition: all 0.25s ease-out;
+        transition: all 0.25s ease-out;
+    }
+    </style>`;
 
     const progress = document.getElementById('loading-bar');
 
