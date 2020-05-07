ae612c365ce1344bfdf92900c10b8c97e75dd957
Antonio Ruggiero <antonio.ruggiero@uqido.com>
2020-05-07 12:37:57 +0200

    Pagine Bostel

---
+++ js/bostelMain.js
@@ -0,0 +1,45 @@
+
+var contentRoot;
+
+function getContentRoot(){
+    contentRoot=document.getElementById("modelContentRoot")
+
+}
+
+function instantiateModelContent(srcUrl,assetName){
+    const element = document.createElement("a");
+    element.classList.add("image-element");
+    element.style.backgroundImage=`url('img/Bostel/${assetName}.jpg')`;
+    element.href=`${srcUrl}/${assetName}.html`;
+    contentRoot.appendChild(element);
+}
+
+function isLocalhost(){
+    return !window.location.host.replace(":8080", "").replace(/localhost|127\.0\.0\.1/i, '');
+}
+
+function parseJson(json){
+    let baseurl = json.baseUrl;
+    if(isLocalhost()){
+        console.log("Is Localhost!")
+        baseurl="";
+    }else{
+        console.log("Is not Localhost!")
+    }
+    console.log("Base Url:"+baseurl);
+    const assets = json.assets_bostel;
+    getContentRoot();
+    for(asset of assets){
+        instantiateModelContent(`${baseurl}`,asset);
+    }
+}
+
+
+function loadJson(){
+    //read json
+    fetch("./assets.json")
+    .then(response => response.json())
+    .then(json => parseJson(json))
+}
+
+loadJson();
