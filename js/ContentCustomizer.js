b22bda21aa11373ea914aa298bfce456af9a3f42
Antonio Ruggiero <antonio.ruggiero@uqido.com>
2020-06-01 18:08:09 +0200

    Fix color change su safari

--- js/ContentCustomizer.js
+++ js/ContentCustomizer.js
@@ -6,6 +6,7 @@ if (typeof modelviewer === "undefined") {
 }
 
 modelviewer.addEventListener("worklet-created", (event) => {
+    console.log("Worklet-created");
     if (typeof worker === "undefined") {
         window.worker = event.detail.worklet;
     }else {
@@ -34,9 +35,8 @@ function initCallbacks() {
 }
 
 function onClickCallback(event) {
-    if (worker === undefined) return;
+    if (typeof worker === "undefined") return;
     if (NameToColor === undefined) return;
-
     const found=NameToColor.find(elem => Object.keys(elem)[0]===event.target.id)
 
     if (found === undefined) return;
@@ -44,6 +44,8 @@ function onClickCallback(event) {
     const colorString=Object.values(found)[0]
 
     let color = colorToArray(colorString);
+    console.log(color)
+    console.log(worker)
     worker.postMessage({type: "change-color", payload: color})
     clearSelected()
     event.target.classList.add("selected")
@@ -58,9 +60,8 @@ function assignColorToButton(buttonNameAndColor) {
 
 
 function initializeCustomizerOnMaterials(materialsName) {
-    if (modelviewer === undefined) return;
 
-    modelviewer.innerHTML +=`<script type="experimental-scene-graph-worklet"  allow="material-properties;messaging">
+    modelviewer.innerHTML +=`<script type="experimental-scene-graph-worklet" allow="material-properties;messaging" >
 
         console.log('Hello from the scene graph worklet!');
 
@@ -73,15 +74,7 @@ function initializeCustomizerOnMaterials(materialsName) {
         });
     
         self.addEventListener('message', (event) => {
-            /*if(event.data.type==="toggle-visibility"){
-                if(ColoredMat.visible===true){
-                    console.log('Become invisible');
-                    ColoredMat.setVisible(false);
-                }else{
-                    console.log('Become visible');
-                    ColoredMat.setVisible(true);
-                }
-            }else */if(event.data.type==="change-color"){
+            if(event.data.type==="change-color"){
                 console.log('Changing color to:', event.data.payload);
                 for(let mat of materials){
                     mat.pbrMetallicRoughness.setBaseColorFactor(event.data.payload);
